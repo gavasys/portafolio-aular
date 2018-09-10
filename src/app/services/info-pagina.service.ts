6449b649220297ef1c03d36240e8198726fb71fb
Gerson Argueta <gava_sys@yahoo.com>
2018-09-10 16:39:34 -0600

    Template listo hasta json

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  constructor(private http: HttpClient) {
+  // console.log('Servicio de infoPagina listo');
+
+  // Leer el archivo JSON
+  this.http.get('assets/data/data-pagina.json')
+    .subscribe( resp => {
+      console.log(resp);
+    });
+
+  }
+}
